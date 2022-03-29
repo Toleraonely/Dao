44b4b062e291787844302eaef1f87831fdb3cd43
Toleraonely <295169157@qq.com>
2022-03-29 19:09:37 +0800

    Dao

---
+++ Dao/hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x65cCd1964E418b12d828CFcc91705412D90Fe715";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
